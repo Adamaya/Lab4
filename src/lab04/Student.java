5779f8c963f527951efcc376968941f64db3e1d1
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-09-04 18:56:41 +0530

    getter setter created

--- src/lab04/Student.java
+++ src/lab04/Student.java
@@ -4,5 +4,21 @@ public class Student extends Person {
     private long uniRollNumber;
     private double currentCPI;
 
+    public long getUniRollNumber() {
+        return uniRollNumber;
+    }
+
+    public void setUniRollNumber(long uniRollNumber) {
+        this.uniRollNumber = uniRollNumber;
+    }
+
+    public double getCurrentCPI() {
+        return currentCPI;
+    }
+
+    public void setCurrentCPI(double currentCPI) {
+        this.currentCPI = currentCPI;
+
+    }
 
 }
