b405301277b9c294ce318b1f556143f3aec22535
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-09-04 19:08:03 +0530

    Student object created

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,13 @@
 package main;
 
 import lab04.Person;
+import lab04.Student;
 
 class Main {
     public static void main(String[] args) {
         Person p1 = new Person("Dean", 40, 'm');
 
+        Student student = new Student(p1.getName(), p1.getAge(), p1.getGender(), 1234567890L, 6.5);
 
     }
 }
