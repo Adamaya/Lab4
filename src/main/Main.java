a5333e42eaf390f050feb7ac7eb09bb453a4dbb6
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-09-04 19:08:40 +0530

    printing student.toString()

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,7 +8,10 @@ class Main {
         Person p1 = new Person("Dean", 40, 'm');
 
         Student student = new Student(p1.getName(), p1.getAge(), p1.getGender(), 1234567890L, 6.5);
-
+        System.out.println(student.toString());
+//        Person person = new Person("abc", 12, 'm');
+//        Student student = new Student("nbek0", 67, 'f', 123L, 0.0);
+        //dynamic binding dispatch
     }
 }
 
